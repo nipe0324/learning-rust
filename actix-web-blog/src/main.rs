194ca60d24299f2b243c1436f9fb869f6a9a60c7
Shojiro Yanagisawa <nipe0324@gmail.com>
2022-11-07 22:33:26 +0900

    actix-web-blog: post /posts

--- actix-web-blog/src/main.rs
+++ actix-web-blog/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+mod error;
+mod repository;
+mod schema;
+
+use actix_web::{web, App, HttpResponse, HttpServer};
+use error::ApiError;
+use repository::{NewPost, Repository};
+
+#[actix_web::post("/posts")]
+async fn create_post(
+    repo: web::Data<Repository>,
+    new_post: web::Json<NewPost>,
+) -> Result<HttpResponse, ApiError> {
+    let new_post = new_post.into_inner();
+    let post = repo.create_post(new_post).await?;
+    Ok(HttpResponse::Ok().json(post))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let database_url = std::env::var("DATABASE_URL").unwrap();
+    let repo = web::Data::new(Repository::new(&database_url));
+
+    HttpServer::new(move || App::new().app_data(repo.clone()).service(create_post))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
---
+++ actix-web-blog/src/repository.rs
@@ -0,0 +1,62 @@
+use crate::error::ApiError;
+use crate::schema::*;
+use actix_web::web;
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+use diesel::sqlite::SqliteConnection;
+use serde::{Deserialize, Serialize};
+
+type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+pub struct Repository {
+    pool: DbPool,
+}
+
+impl Repository {
+    pub fn new(database_url: &str) -> Self {
+        let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+        let pool = r2d2::Pool::builder()
+            .build(manager)
+            .expect("Failed to create a pool.");
+        Self { pool }
+    }
+
+    pub async fn create_post(&self, new_post: NewPost) -> Result<Post, ApiError> {
+        let mut conn = self.pool.get()?;
+        let post = web::block(move || {
+            diesel::insert_into(posts::table)
+                .values(new_post)
+                .get_result(&mut conn)
+        })
+        .await??;
+
+        Ok(post)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_conn() {
+        let database_url = std::env::var("DATABASE_URL").unwrap();
+        let repo = Repository::new(&database_url);
+        assert!(repo.pool.get().is_ok());
+    }
+}
+
+#[derive(Deserialize, Insertable)]
+#[diesel(table_name = posts)]
+pub struct NewPost {
+    title: String,
+    body: String,
+}
+
+#[derive(Serialize, Queryable)]
+pub struct Post {
+    id: i32,
+    title: String,
+    body: String,
+    published: bool,
+}
