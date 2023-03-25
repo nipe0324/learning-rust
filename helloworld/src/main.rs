7021aa3cf17b7e750fa298deb6a610a738669bfe
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-25 10:24:23 +0900

    [helloworld] add test

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn plus(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[test]
+fn plus_test() {
+    assert_eq!(plus(4, 5), 9);
+    assert_eq!(plus(100, -1), 99);
+    assert_eq!(plus(114000, 514), 114514);
+}
