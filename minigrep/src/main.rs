e39103b624b302ba3586865a1dddf91867359fa5
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-29 00:36:19 +0900

    [minigrep] refactor Config and parse_config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,17 +7,30 @@ fn main() {
     // 取り扱いは大変だが、args_os()を使えば不正なユニコードを受け付けることができる
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!(
+        "Searching for '{}' in '{}'\n",
+        config.query, config.filename
+    );
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
