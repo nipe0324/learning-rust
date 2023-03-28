872cc4c2620f62a65daac59cd8c391daf244ab40
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-29 00:41:27 +0900

    [minigrep] handle args length

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -7,7 +8,10 @@ fn main() {
     // 取り扱いは大変だが、args_os()を使えば不正なユニコードを受け付けることができる
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parseing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!(
         "Searching for '{}' in '{}'\n",
@@ -29,10 +33,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
