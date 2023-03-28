c70f468de3f0039da51e1abfab9d1c3e994c9174
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-29 00:35:47 +0900

    [minigrep] print file contents

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     // NOTE: args()は不正なユニコードを含むとパニックを起こす
@@ -10,4 +12,12 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
