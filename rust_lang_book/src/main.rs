bead00749b7271b412529b8411dd80ae99062ca1
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-29 20:19:46 +0900

    [rust_lang_book] closure with capture

--- rust_lang_book/src/main.rs
+++ rust_lang_book/src/main.rs
@@ -1,71 +1,13 @@
-use std::collections::HashMap;
-use std::thread;
-use std::time::Duration;
-
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+    let x = vec![1, 2, 3];
 
-    generate_workout(simulated_user_specified_value, simulated_random_number);
-}
+    // クロージャのequal_to_xはx変数を利用できる
+    let equal_to_x = |z| z == x;
 
-// fn simulated_expensive_calculation(intensity: u32) -> u32 {
-//     println!("calculated slowly...");
-//     thread::sleep(Duration::from_secs(2));
-//     intensity
-// }
+    // move を利用すると、所有権を移動させることがｄけいる
+    // let equal_to_x = move |z| z == x;
 
-fn generate_workout(intensity: u32, random_number: u32) {
-    // let expensive_result = simulated_expensive_calculation(intensity);
-    let mut expensive_result = Cacher::new(|num| {
-        println!("calculating slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    });
+    let y =  vec![1, 2, 3];
 
-    if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_result.value(intensity));
-        println!("Next, do {} psitups!", expensive_result.value(intensity));
-    } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated!");
-        } else {
-            println!(
-                "Today, run for {} minutes",
-                expensive_result.value(intensity)
-            );
-        }
-    }
-}
-
-// closureの結果をキャッシュする構造体
-struct Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    calculation: T,
-    values: HashMap<u32, u32>,
-}
-
-impl<T> Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    fn new(calculation: T) -> Cacher<T> {
-        Cacher {
-            calculation,
-            values: HashMap::new(),
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.values.get(&arg) {
-            Some(v) => *v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.values.insert(arg, v);
-                v
-            }
-        }
-    }
+    assert!(equal_to_x(y));
 }
