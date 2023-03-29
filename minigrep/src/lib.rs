6d48375d4d843baa9a9d8d613b6e334b1e4288b6
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-29 13:35:43 +0900

    [minigrep] add search_case_insensitive

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -47,6 +47,19 @@ pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -59,9 +72,35 @@ Rust:
 safe, fast, productive.
 Pick three.";
 
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_senstive() {
+        let query = "duct";
+
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insenstive() {
+        let query = "rUsT";
+
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
         assert_eq!(
-            vec!["safe, fast, productive."],
-            search(query, contents)
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
