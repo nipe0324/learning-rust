a5b7187fe4dc3e6c9ae4ffc1bb2d28627f59cdfb
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-29 20:49:34 +0900

    [minigrep] use iterator

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -9,9 +9,7 @@ use minigrep::Config;
 fn main() {
     // NOTE: args()は不正なユニコードを含むとパニックを起こす
     // 取り扱いは大変だが、args_os()を使えば不正なユニコードを受け付けることができる
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parseing arguments: {}", err);
         process::exit(1);
     });
