32eda649c46ae9acc7b9a23c20a16775604a5265
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-04-02 19:31:58 +0900

    [webserver] GET /sleep for slow simulation

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,7 +1,9 @@
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::net::TcpListener;
 use std::fs::File;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -18,8 +20,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "static/hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "static/hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "static/404.html")
