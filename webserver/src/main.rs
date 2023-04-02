02c136f92ed82a7d4df215c45e2cfd2142ee51ee
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-04-02 18:12:36 +0900

    [webserver] response html

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -14,8 +15,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let mut file = File::open("static/hello.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
