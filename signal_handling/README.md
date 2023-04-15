5793cd9ff9cc361b704a0e263c209afa626e8740
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-04-15 20:41:25 +0900

    [signal_handling] add ctrl-c handling by ctrlc

---
+++ signal_handling/README.md
@@ -0,0 +1,3 @@
+# Signal handling
+
+- use https://crates.io/crates/ctrlc for Ctrl-C handler
---
+++ signal_handling/src/main.rs
@@ -0,0 +1,13 @@
+use ctrlc;
+use std::sync::mpsc::channel;
+
+fn main() {
+    let (tx, rx) = channel();
+
+    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+        .expect("Error setting Ctrl-C handler");
+
+    println!("Waiting for Ctrl-C...");
+    rx.recv().expect("Could not receive from channel.");
+    println!("Got it! Exiting...");
+}
