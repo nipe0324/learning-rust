093b0c9f357e9deb17f67c6952afff27be7f8cac
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-04-18 21:56:10 +0900

    [rustwi] html response with askama

--- rustwi/README.md
+++ rustwi/README.md
@@ -2,7 +2,8 @@
 
 - https://rust-webapp-tutorial.teruru.net/
 
-Libraries
+## Libraries
 
-- https://github.com/tokio-rs/axum : axum is a web application framework that focuses on ergonomics and modularity.
-- https://github.com/tokio-rs/tokio : Tokio is an event-driven, non-blocking I/O platform for writing asynchronous applications with the Rust programming language
+- [axum](https://github.com/tokio-rs/axum) :  is a web application framework that focuses on ergonomics and modularity.
+- [askama](https://github.com/djc/askama) : Type-safe, compiled Jinja-like templates for Rust
+- [tokio](https://github.com/tokio-rs/tokio) : Tokio is an event-driven, non-blocking I/O platform for writing asynchronous applications with the Rust programming language
--- rustwi/src/main.rs
+++ rustwi/src/main.rs
@@ -1,4 +1,9 @@
-use axum::{response::Html, routing::get, Router};
+use askama::Template;
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use std::net::SocketAddr;
 
 #[tokio::main] // main関数を非同期関数にするために必要
@@ -11,13 +16,34 @@ async fn main() {
     let app = Router::new().route("/", get(handler));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listing on {}", addr);
+    tracing::debug!("listing on http://{}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("Hello, World!")
+async fn handler() -> impl IntoResponse {
+    let tweets = (1..=20)
+        .into_iter()
+        .map(|_| TweetView {
+            name: "太郎".to_string(),
+            message: "こんにちは！".to_string(),
+            posted_at: "2020-01-01 12:34".to_string(),
+        })
+        .collect();
+    let home = Home { tweets };
+    Html(home.render().unwrap()).into_response()
+}
+
+struct TweetView {
+    name: String,
+    message: String,
+    posted_at: String,
+}
+
+#[derive(Template)]
+#[template(path = "home.html")]
+struct Home {
+    tweets: Vec<TweetView>,
 }
