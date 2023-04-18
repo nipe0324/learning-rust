68a33ba69721eb77cc378351f1eaf5de051a26bd
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-04-18 21:10:59 +0900

    [rustwi] hello, world htttp response with axum

---
+++ rustwi/README.md
@@ -0,0 +1,8 @@
+# Rust Webapp Tutorial
+
+- https://rust-webapp-tutorial.teruru.net/
+
+Libraries
+
+- https://github.com/tokio-rs/axum : axum is a web application framework that focuses on ergonomics and modularity.
+- https://github.com/tokio-rs/tokio : Tokio is an event-driven, non-blocking I/O platform for writing asynchronous applications with the Rust programming language
---
+++ rustwi/src/main.rs
@@ -0,0 +1,23 @@
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main] // main関数を非同期関数にするために必要
+async fn main() {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "rustwi=debug")
+    }
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listing on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("Hello, World!")
+}
