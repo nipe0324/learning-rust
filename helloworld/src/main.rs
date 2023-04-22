03fa7a1f561225814b4ad673e1cab6e7b1953511
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-04-22 17:52:56 +0900

    [helloworld] update hello world

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -2,6 +2,7 @@ use std::num::ParseIntError;
 
 fn main() {
     println!("Hello, world!");
+    println!("1 + 2 = {}", plus(1, 2));
 }
 
 fn plus(a: i32, b: i32) -> i32 {
@@ -13,15 +14,4 @@ fn plus_test() {
     assert_eq!(plus(4, 5), 9);
     assert_eq!(plus(100, -1), 99);
     assert_eq!(plus(114000, 514), 114514);
-}
-
-fn parse_and_plus(s: &str, n: i32) -> Result<i32, ParseIntError> {
-    let parsed_int = s.parse::<i32>()?;
-    Ok(parsed_int + n)
-}
-
-#[test]
-fn parse_and_plus_test() {
-    assert_eq!(parse_and_plus("100", 1).unwrap(), 101);
-    assert!(parse_and_plus("abc", 1).is_err());
 }
