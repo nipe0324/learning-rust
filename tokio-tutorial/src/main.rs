0e68fb536160fc9346df81223c73a78834b51ee9
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-04-28 08:17:55 +0900

    [tokio-tutorial] shared state

--- tokio-tutorial/src/main.rs
+++ tokio-tutorial/src/main.rs
@@ -1,38 +1,50 @@
 use tokio::net::{TcpListener, TcpStream};
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use mini_redis::{Connection, Frame};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // リスナーを 127.0.0.1:6379 にバインドする
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // クライアントからの接続を待つ
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
 
+        println!("Accepted");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
 
-    let mut db = HashMap::new();
-
+    // mini-redisによって提供されたConnection型を使用して、
+    // ソケットからフレームをパースする
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
