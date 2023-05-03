b4080913abf8e2a57d10d6be7837f5ac5d30ad2d
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-05-03 10:34:27 +0900

    [axum-sqlx] add test_slugify

--- axum-sqlx/src/http/articles/mod.rs
+++ axum-sqlx/src/http/articles/mod.rs
@@ -161,3 +161,21 @@ fn slugify(string: &str) -> String {
         })
         .join("-")
 }
+
+#[test]
+fn test_slugify() {
+    assert_eq!(
+        slugify("Segfaults and You: When Raw Pointers Go Wrong"),
+        "segfaults-and-you-when-raw-pointers-go-wrong"
+    );
+
+    assert_eq!(
+        slugify("Why are DB Admins Always Shouting?"),
+        "why-are-db-admins-always-shouting"
+    );
+
+    assert_eq!(
+        slugify("Converting to Rust from C: It's as Easy as 1, 2, 3!"),
+        "converting-to-rust-from-c-its-as-easy-as-1-2-3"
+    )
+}
