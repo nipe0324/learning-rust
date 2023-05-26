5a751e44f0f5d754b7191a7ce0198cf44bb594e6
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-05-26 15:18:24 +0900

    [blog_os] custom test runner

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,5 +1,8 @@
 #![no_std] // OSの機能に依存している標準ライブラリにリンクしない
 #![no_main] // 通常のエントリポイントを使わないようにする
+#![feature(custom_test_frameworks)] // カスタムテストフレームワークを使うための機能
+#![test_runner(crate::test_runner)] // カスタムテストランナーを使う
+#![reexport_test_harness_main = "test_main"] // テストランナーのエントリポイントを`test_main`にする
 
 use core::panic::PanicInfo;
 
@@ -10,7 +13,10 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    // panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -19,4 +25,19 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
