2efaf6f2123f8ccdc0bc8a09a781ac692b68d5d4
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-05-26 15:47:32 +0900

    [blog_os] timeout test & output test

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -56,11 +56,26 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
 
     exit_qemu(QemuExitCode::Success);
@@ -68,8 +83,6 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
     // assert_eq!(0, 1);
-    serial_println!("[ok]");
 }
