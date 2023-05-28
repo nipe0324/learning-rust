65f5bd4ef6f2f231cf855fccb1c97b8d7dee1668
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-05-28 11:46:18 +0900

    [blog_os] init idt

--- blog_os/src/interrupts.rs
+++ blog_os/src/interrupts.rs
@@ -1,10 +1,19 @@
-use crate::println;
+use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+use crate::println;
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
 
 // Interrupt Descriptor Table(IDT)を初期化する
 pub fn init_idt() {
-    let mut idt = InterruptDescriptorTable::new();
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
+    IDT.load();
 }
 
 // ブレークポイント例外のハンドラー
