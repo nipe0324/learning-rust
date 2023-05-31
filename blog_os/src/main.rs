dfbb37005f49bf6b8d313c5823e903bf6b6fa54c
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-05-31 22:49:15 +0900

    [blog_os] level 4 page table address

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -15,10 +15,13 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    let ptr = 0x00 as *mut u32;
-    unsafe {
-        *ptr = 42;
-    }
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!(
+        "Level 4 page table at: {:?}",
+        level_4_page_table.start_address(),
+    );
 
     #[cfg(test)]
     test_main();
