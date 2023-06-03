e45ea8df8bfd61ee6e92fb0d084c374689e151d7
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-06-03 23:28:31 +0900

    [blog_os] translate vertual addr to phsycal addr

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -5,23 +5,37 @@
 #![reexport_test_harness_main = "test_main"] // テストランナーのエントリポイントを`test_main`にする
 
 use blog_os::println;
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use bootloader::{BootInfo, entry_point};
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    println!("Hello World{}", "!");
+    use blog_os::memory::translate_addr;
+    use x86_64::structures::paging::PageTable;
+    use x86_64::VirtAddr;
 
+    println!("Hello World{}", "!");
     blog_os::init();
 
-    use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!(
-        "Level 4 page table at: {:?}",
-        level_4_page_table.start_address(),
-    );
+    let addresses = [
+        // 恒等対応しているVGAバッファのページ
+        0xb8000,
+        // コードページのどこか
+        0x201008,
+        // スタックページのどこか
+        0x0100_0020_1a10,
+        // 物理アドレス "0" にマップされている仮想アドレス
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
---
+++ blog_os/src/memory.rs
@@ -0,0 +1,54 @@
+use x86_64::{structures::paging::PageTable, PhysAddr, VirtAddr};
+
+/// 有効なレベル4テーブルへの可変参照を返す
+pub unsafe fn active_level_4_table(phsical_memory_offset: VirtAddr) -> &'static mut PageTable {
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let phys = level_4_table_frame.start_address();
+    let virt = phsical_memory_offset + phys.as_u64();
+    let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
+
+    &mut *page_table_ptr // unsafe
+}
+
+/// 仮想アドレスを対応する物理アドレスに変換する。
+/// アドレスがマップされていないなら`None`を返す
+pub unsafe fn translate_addr(addr: VirtAddr, phsical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    translate_addr_inner(addr, phsical_memory_offset)
+}
+
+fn translate_addr_inner(addr: VirtAddr, phsical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    use x86_64::registers::control::Cr3;
+    use x86_64::structures::paging::page_table::FrameError;
+
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let table_indexes = [
+        addr.p4_index(),
+        addr.p3_index(),
+        addr.p2_index(),
+        addr.p1_index(),
+    ];
+
+    let mut frame = level_4_table_frame;
+
+    // 複数層のページテーブルをたどる
+    for &index in &table_indexes {
+        // フレームをページテーブルの参照に変換する
+        let virt = phsical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe { &*table_ptr };
+
+        // ページテーブルエントリを読んで、`frame`を更新する
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
+        };
+    }
+
+    Some(frame.start_address() + u64::from(addr.page_offset()))
+}
