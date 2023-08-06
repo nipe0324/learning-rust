39bdd1f7e8ca7d5b38b664bd5209d8bf5ffd93af
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-08-06 11:28:56 +0900

    [try_thiserror] not use thiserror

--- try_thiserror/src/main.rs
+++ try_thiserror/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+// use std::fmt;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -12,6 +13,32 @@ pub enum DataStoreError {
     #[error("unknown data store error")]
     Unknown,
 }
+
+// not use thiserror
+// #[derive(Debug)]
+// pub enum DataStoreError {
+//     Disconnect(io::Error),
+//     Redaction(String),
+//     InvalidHeader { expected: String, found: String },
+//     Unknown,
+// }
+
+// impl fmt::Display for DataStoreError {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         match self {
+//             Self::Disconnect(_) => write!(f, "data store disconnected"),
+//             Self::Redaction(s) => write!(f, "the data for key `{s}` is not available"),
+//             Self::InvalidHeader { expected, found }  => write!(f, "invalid header (expected {expected:?}, found {found:?})"),
+//             Self::Unknown => write!(f, "unknown data store error"),
+//         }
+//     }
+// }
+
+// impl From<io::Error> for DataStoreError {
+//     fn from(err: io::Error) -> Self {
+//         DataStoreError::Disconnect(err)
+//     }
+// }
 
 fn connect_data_store() -> Result<(), io::Error> {
     Err(io::Error::from(io::ErrorKind::ConnectionAborted))
